0d88baaa0c548a55f55b463f55d29c5e0c3b1a88
Josh Seidman <75498758+SideofMan@users.noreply.github.com>
2022-11-09 09:51:12 -0600

    Cleaning up testing file
    Slightly easier to navigate now!

--- testing.R
+++ testing.R
@@ -4,7 +4,9 @@ library(tictoc)
 library(ggplot2)
 library(jpeg)
 
-# set wd to reduced images
+
+# set wd to reduced images ------------------------------------------------
+
 # jdseidma for desktop, thema for laptop
 
 reduced_dir <- "C:/Users/jdseidma/Dropbox/Topics in Math Stats 5931/Final Project/Images/Reduced Images"
@@ -14,17 +16,14 @@ folders <- dir(path = reduced_dir, pattern = NULL, all.files = FALSE,
                full.names = FALSE, recursive = FALSE,
                ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 
-# photos <- dir(path = paste0(reduced_dir, "/",folders[1]), pattern = NULL, all.files = FALSE,
-#               full.names = FALSE, recursive = FALSE,
-#               ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 
-# setwd(paste0(reduced_dir, "/",folders[1]))
-
-# individual picture converted to vector
+# individual picture converted to vector (for testing) --------------------
 
 # set n = # pixels for width and height
 
 n = 100
+
+# importing a picture into R
 
 setwd(paste0(reduced_dir, "/",folders[1]))
 photos <- dir(path = paste0(reduced_dir, "/",folders[1]), 
@@ -32,6 +31,9 @@ photos <- dir(path = paste0(reduced_dir, "/",folders[1]),
               full.names = FALSE, recursive = FALSE,
               ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 pic <- grayscale(load.image(photos[1]))
+
+# resizing it to nxn
+
 img <- resize(pic, n, n)
 vector <- as.data.frame(img)[,3]
 
@@ -42,7 +44,8 @@ plot(pic)
 plot(img)
 
 
-# converting picture to vector function for coding checks 
+# converting picture to vector function for coding checks  ----------------
+
 # (not utilized in the for loop to create matrix of images)
 
 pic_to_vector <- function(i, j){
@@ -57,15 +60,9 @@ pic_to_vector <- function(i, j){
   return(vector)
 }
 
-# name <- paste0(reduced_dir, "/",folders[1])
-# photos <- dir(path = paste0(reduced_dir, "/",folders[i]), 
-#               pattern = NULL, all.files = FALSE,
-#               full.names = FALSE, recursive = FALSE,
-#               ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-# length(photos)
+# Putting data into matrix -----------------------------------------
 
-
-## CREATING MATRIX OF IMAGES (nxn)
+# CREATING MATRIX OF IMAGES (nxn)
 
 X <- c()
 
@@ -86,13 +83,13 @@ for (i in 1:length(folders)) {
 }
 toc()
 
-# writing as a CSV file to prevent running above code every time
+# writing as a CSV file to allow us to not have to run above code every time
 
 tic("writing csv")
 write.csv(X,"C:/Users/jdseidma/Dropbox/Topics in Math Stats 5931/Final Project\\imagematrix.csv", row.names = FALSE)
 toc()
 
-# loading CSV file into R
+# loading CSV file into R (set wd to wherever it's saved)
 
 setwd("C:/Users/jdseidma/Dropbox/Topics in Math Stats 5931/Final Project")
 
@@ -106,7 +103,7 @@ meanface <- matrix(rowMeans(X), nrow = n)
 
 writeJPEG(t(meanface), 'meanface.jpg')
 
-# full PCA of image matrix -- need to do pca of transpose for some reason??
+# full PCA of image matrix -- need to do pca of transpose for some --------
 
 tic("pca runtime")
 faces_pca <- prcomp(t(X), center = TRUE, scale. = TRUE)
@@ -128,13 +125,7 @@ for (i in 1:ncol(importance)) {
   }
 }
 
-# new pca with only first r principal components
-
-tic("reduced pca runtime")
-reduced_faces_pca <- prcomp(X2, center = TRUE, scale. = TRUE, rank. = r)
-toc()
-
-# scree plot for PCA
+# scree plot for PCA (first 10 components)
 
 var_explained = faces_pca$sdev^2 / sum(faces_pca$sdev^2)
 qplot(c(1:10), var_explained[1:10]) +
@@ -144,26 +135,8 @@ qplot(c(1:10), var_explained[1:10]) +
   ggtitle("Scree Plot") +
   ylim(0, 1)
 
-# pca_img <- as.cimg(pca_pic$x[,1:50]%*%t(pca_pic$rotation[,1:50]))
-# plot(pca_img)
-# plot(pic)
-# object.size(pic_mat)
-# object.size(pca_pic$x[,1:50])
-# 
-# compressed <- pca_pic$x[,1:250]%*%t(pca_pic$rotation[,1:250])
-# 
-# writeJPEG(compressed, 'test_pca.jpg')
-# 
-# image_plot <- function(path, plot_name) {
-#   require('jpeg')
-#   img <- readJPEG(path)
-#   d <- dim(img)
-#   plot(0,0,xlim=c(0,d[2]),ylim=c(0,d[2]),xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
-#   title(plot_name, line = -0.5)
-#   rasterImage(img,0,0,d[2],d[2])
-# }
-# 
-# image_plot('test_pca.jpg', '50 components')
+
+# plotting Eigenfaces and reconstructing images ---------------------------
 
 
 # why is each column of rotation not an eigenface???
@@ -183,9 +156,6 @@ writeJPEG(ef_2_mat, 'test_ef_2.jpg')
 ef_3 <- matrix(EigenFaces[,3], ncol = 1)
 ef_3_mat <- matrix(ef_3, nrow = n)
 writeJPEG(ef_3_mat, 'test_ef_3.jpg')
-
-
-## to plot eigenfaces and reconstruct
 
 # eigenfaces: make a matrix of the column of the rotation matrix (defined above)
 
