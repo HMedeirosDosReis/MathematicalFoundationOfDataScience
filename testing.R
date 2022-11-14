bee1ae9c6aa46f65894830ce4370ed7f8077faa1
JenniferSailor <117314279+JenniferSailor@users.noreply.github.com>
2022-11-14 15:29:20 -0600

    11/14

--- testing.R
+++ testing.R
@@ -3,50 +3,63 @@ library(imager)
 library(tictoc)
 library(ggplot2)
 library(jpeg)
+library(magick)
 
 
 # set wd to reduced images ------------------------------------------------
 
 # jdseidma for desktop, thema for laptop
 
-reduced_dir <- "C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project/Images/Reduced Images"
+#C:/Users/jans7/OneDrive - Marquette University/Fall 2022/MSSC 5931 - Topics in Math or Stats/Project/NewFace_20
+#reduced_dir <- "C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project/Images/Reduced Images"
+reduced_dir <- "C:/Users/jans7/OneDrive - Marquette University/Fall 2022/MSSC 5931 - Topics in Math or Stats/Project/NewFace_20"
 setwd(reduced_dir)
+
+if (length(list.files(reduced_dir)) == 0){
+  stop("Empty Folder")
+}
 
 folders <- dir(path = reduced_dir, pattern = NULL, all.files = FALSE,
                full.names = FALSE, recursive = FALSE,
                ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 
+#change to 65 when we add ourselves
+if (length(folders) < 62){
+  stop("Missing atleast 1 folder")
+}
 
+#-----------------------------------
 # individual picture converted to vector (for testing) --------------------
 
 # set n = # pixels for widths and heights
 
-n = 100
-
-# importing a picture into R
-
-setwd(paste0(reduced_dir, "/",folders[1]))
-photos <- dir(path = paste0(reduced_dir, "/",folders[1]), 
-              pattern = NULL, all.files = FALSE,
-              full.names = FALSE, recursive = FALSE,
-              ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-pic <- grayscale(load.image(photos[1]))
-
-# resizing it to nxn
-
-img <- resize(pic, n, n)
-vector <- as.data.frame(img)[,3]
-
-# comparing 250x250 image to nxn image
-
-layout(t(c(1:2)))
-plot(pic)
-plot(img)
-
+# n = 100
+# 
+# # importing a picture into R
+# 
+# setwd(paste0(reduced_dir, "/",folders[1]))
+# photos <- dir(path = paste0(reduced_dir, "/",folders[1]), 
+#               pattern = NULL, all.files = FALSE,
+#               full.names = FALSE, recursive = FALSE,
+#               ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+# pic <- grayscale(load.image(photos[1]))
+# 
+# # resizing it to nxn
+# 
+# img <- resize(pic, n, n)
+# vector <- as.data.frame(img)[,3]
+# 
+# # comparing 250x250 image to nxn image
+# 
+# layout(t(c(1:2)))
+# plot(pic)
+# plot(img)
+#------------------------------------------
 
 # converting picture to vector function for coding checks  ----------------
 
 # (not utilized in the for loop to create matrix of images)
+n = 100
 
 pic_to_vector <- function(i, j){
   setwd(paste0(reduced_dir, "/",folders[i]))
@@ -59,6 +72,7 @@ pic_to_vector <- function(i, j){
   vector <- as.data.frame(pic)[,3]
   return(vector)
 }
+
 
 # Putting data into matrix -----------------------------------------
 
@@ -83,19 +97,23 @@ for (i in 1:length(folders)) {
 }
 toc()
 
+#writing it out to a csv takes alot longer (increases size of memory) so we will no longer do it
 # writing as a CSV file to allow us to not have to run above code every time
 
-tic("writing csv")
-write.csv(X,"C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project\\imagematrix.csv", row.names = FALSE)
-toc()
+# tic("writing csv")
+# #write.csv(X,"C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project\\imagematrix.csv", row.names = FALSE)
+# write.csv("C:/Users/jans7/OneDrive - Marquette University/Fall 2022/MSSC 5931 - Topics in Math or Stats/Project\\imagematrix.csv", row.names = FALSE)
+# toc()
+
+
 
 # loading CSV file into R (set wd to wherever it's saved)
 
-setwd("C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project")
-
-tic("loading in matrix")
-X2 <- read.csv('imagematrix.csv')
-toc()
+# setwd("C:/Users/jans7/OneDrive - Marquette University/Fall 2022/MSSC 5931 - Topics in Math or Stats/Project")
+# 
+# tic("loading in matrix")
+# X2 <- read.csv('imagematrix.csv')
+# toc()
 
 # plotting mean face for fun (he don't look great)
 
@@ -141,9 +159,11 @@ qplot(c(1:10), var_explained[1:10]) +
 
 # why is each column of rotation not an eigenface???
 
-setwd("C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project")
+#setwd("C:/Users/thema/Dropbox/Topics in Math Stats 5931/Final Project")
+setwd("../..")
 
-EigenFaces = faces_pca$rotation[,1:r]
+#first 3 eigenfaces
+EigenFaces <- faces_pca$rotation[,1:r]
 
 ef_1 <- matrix(EigenFaces[,1], ncol = 1)
 ef_1_mat <- matrix(ef_1, nrow = n)
@@ -170,22 +190,68 @@ restr <- faces_pca$x[,1:r]%*%t(EigenFaces)
 restr <- scale(restr, center = -1*faces_pca$center, scale = 1/faces_pca$scale)
 
 # to plot these, need to rerun code at beginning of document (copied)
+#change to 65 once we add our faces
+rperson <- sample(1:62, 1)
 
-setwd(paste0(reduced_dir, "/",folders[1]))
-photos <- dir(path = paste0(reduced_dir, "/",folders[1]), 
+
+
+setwd(paste0(reduced_dir, "/",folders[rperson]))
+photos <- dir(path = paste0(reduced_dir, "/",folders[rperson]), 
               pattern = NULL, all.files = FALSE,
               full.names = FALSE, recursive = FALSE,
               ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-pic <- grayscale(load.image(photos[2]))
+
+rpic <- sample(1:length(photos), 1)
+
+pic <- grayscale(load.image(photos[rpic]))
 
 img <- resize(pic, n, n)
 
+place <- 0
+
+for (i in 1:(rperson-1)) {
+  setwd(paste0(reduced_dir, "/",folders[i]))
+  photos <- dir(path = paste0(reduced_dir, "/",folders[i]), 
+                pattern = NULL, all.files = FALSE,
+                full.names = FALSE, recursive = FALSE,
+                ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+  place <- length(photos) + place
+}
+
+place <- place + rpic
 
 layout(t(1:3))
 plot(pic)
-plot(as.cimg(matrix(X[,1], ncol = n)))
-plot(as.cimg(matrix(restr[1,], ncol = n)))
+plot(as.cimg(matrix(X[,place], ncol = n)))
+plot(as.cimg(matrix(restr[place,], ncol = n)))
+title(name_associated_w_pic_num(reduced_dir, place))
 
+# LR ------------------------------
+
+tx<-t(X)
+y<-c()
+
+for (i in 1:length(folders)) {
+  setwd(paste0(reduced_dir, "/",folders[i]))
+  photos <- dir(path = paste0(reduced_dir, "/",folders[i]), 
+                pattern = NULL, all.files = FALSE,
+                full.names = FALSE, recursive = FALSE,
+                ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+  y <- c(y, rep(i, each=length(photos)))
+}
+
+line_2 <- lm(y ~ tx)
+ourpred <- predict(line_2, newdata = data.frame(tx[,place]))
+res <- ourpred[place]
+
+folders[as.numeric(res)]
+folders[as.numeric(res)+1]
+folders[res[[1]]]
+folders[res[1]]
+name_associated_w_pic_num(reduced_dir, 2453)
+
+#plot(as.cimg(matrix(ourpred, ncol = n)))
+#title(title(name_associated_w_pic_num(reduced_dir, place)))
 
 # trying to fix face shade ------------------------------------------------
 
